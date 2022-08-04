1b59a01402988eb650b0f5bd8aae645f9165fa3f
Joao Marcos <joao.marcos.witcel@gmail.com>
2022-08-03 21:28:43 -0300

    concluído e funcionando

--- index.ts
+++ index.ts
@@ -149,15 +149,94 @@ function sequence<T>(parsers: Parser<T>[]): Parser<T[]> {
     }
 }
 
-const cow = str("cow");
-const says = str("says");
-const moo = str("moo");
-const space = str(" ");
 
-const parseCowSentence = sequence([ cow, space, says, space, moo ]);
+// Cenário original
+{
+    const cow = str("cow");
+    const says = str("says");
+    const moo = str("moo");
+    const space = str(" ");
+    
+    const parseCowSentence = sequence([ cow, space, says, space, moo ]);
+    
+    
+    const ctx = { text: "cow says moo", index: 0 };
+    const result = parseCowSentence(ctx);
+    
+    console.log(result);
+}
 
+// Cenário mais completo
+{
+    type Expr = Call | number;
 
-const ctx = { text: "cow says moo", index: 0 };
-const result = parseCowSentence(ctx);
+    interface Call {
+        target: string;
+        args: Expr[];
+    }
 
-console.log(result);
+    /**
+     * Função que recebe o texto a ser parseado e realiza os preparativos,
+     * quando o texto é parseado a função realiza o unboxing do texto.
+     * @param text Texto que tentaremos parsear
+     * @returns AST construída a partir do texto
+     */
+    const parse = (text: string): Expr => {
+        const res = expr({ text, index: 0});
+
+        if (res.success) return res.value;
+        throw `Parse error, expected ${res.expected} at char ${res.ctx.index}`;
+    }
+
+    // expr = call | numberLiteral
+    const expr = (ctx: Context): Result<Expr> => {
+        return any<Expr>([call, numberLiteral])(ctx);
+    }
+
+    // regex que consome identificadores
+    const ident = regex(/[a-zA-Z][a-zA-Z0-9]*/g, "identifier");
+
+    // regex parser que consome um string de número
+    const numberLiteral = map(
+        regex(/[+\-]?[0-9]+(\.[0-9]*)?/g, "number"),
+        // Mapeamento para a função que realizará a conversão da string para um
+        // number, possivelmente com casas decimais
+        parseFloat
+    );
+
+    // traillingArg = ',' arg
+    const trailingArg = map(
+        sequence<any>([str(","), expr]),
+        // mapeado para um função que descarta a vírgula,
+        // retornando apenas a expressão do argumento
+        ([ _comma, argExpr]): Expr[] => argExpr
+    );
+
+    // args = expr ( trailingArg ) *
+    const args = map(
+        sequence<any>([expr, many(trailingArg)]),
+        // Combinando o primeiro argumento e os agumentos restantes em uma
+        // lista única
+        ([arg1, rest]): Expr[] => [arg1,...rest],
+    );
+
+    // call = ident "(" args ")"
+    const call = map(
+        sequence<any>([ident, str("("), optional(args), str(")")]),
+        // Descartamos o parêntese esquerdo e direito e usamos o nome da função
+        // e a lista de argumentos para construir o nó Call da nossa AST
+        ([fnName, _lparen, argList, _rparen]): Call => ({
+            target: fnName,
+            args: argList || []
+        }),
+    );
+
+    const example = (code: string): void => {
+        console.log(JSON.stringify(parse(code), null, 2));
+    }
+      
+    example("1");
+    example("Foo()");
+    example("Foo(Bar())");
+    example("Foo(Bar(1,2,3))");
+}
